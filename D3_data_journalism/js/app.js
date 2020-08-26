2a2d4c0d510730364fa2dd91b18a282f56990e10
Carrie Chunn <crchunn@gmail.com>
2020-08-26 13:11:25 -0500

    finalizing first draft

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -9,18 +9,18 @@ var margin = {
     right: 50,
     bottom: 50,
     left: 50
-  };
-  var height = svgHeight - margin.top - margin.bottom;
-  var width = svgWidth - margin.left - margin.right;
+};
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
 
-  // csv data
-  d3.csv("data.csv").then(function(stateData) {
+// csv data
+d3.csv("data.csv").then(function (stateData) {
 
     // create date parser
     // var dateParser = d3.timeParse("%d-%b");
 
     // parse data
-    stateData.forEach(function(data) {
+    stateData.forEach(function (data) {
         data.poverty = +data.poverty;
         data.age = +data.age;
         data.healthcare = +data.healthcare;
@@ -32,12 +32,12 @@ var margin = {
     // setting up the scale
 
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(stateData, d => d.poverty)])
-      .range([0, width]);
+        .domain([20, d3.max(stateData, d => d.poverty)])
+        .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(stateData, d => d.age)])
-      .range([height, 0]);
+        .domain([0, d3.max(stateData, d => d.age)])
+        .range([height, 0]);
 
     //   axis
     var bottomAxis = d3.axisBottom(xLinearScale);
@@ -51,7 +51,7 @@ var margin = {
 
     chartGroup.append("g")
         .call(leftAxis);
-    
+
     // making the donuts
 
     var circlesGroup = chartGroup.selectAll("circle")
@@ -59,8 +59,8 @@ var margin = {
         .enter()
         .append("circle")
         .attr("cx", d => xLinearScale(d.poverty))
-        .attr("cx", d => xLinearScale(d.healthcare))
-        .attr("cx", d => xLinearScale(d.income))
+        // .attr("cx", d => xLinearScale(d.healthcare))
+        // .attr("cx", d => xLinearScale(d.income))
         .attr("cy", d => yLinearScale(d.age))
         .attr("cy", d => yLinearScale(d.smokes))
         .attr("cy", d => yLinearScale(d.obesity))
@@ -68,8 +68,39 @@ var margin = {
         .attr("fill", "aqua")
         .attr("opacity", ".5");
 
-    
+    // setting up tooltip
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function (d) {
+            return (`${d.abbr}<br>Poverty: ${d.poverty}<br>Age: ${d.age}`);
+        });
 
+    // put tooltip in chart
+    chartGroup.call(toolTip);
 
+    // set up the bugs
 
-  })
+    circlesGroup.on("click", function (data) {
+        toolTip.hide(data);
+    })
+        .on("mouseout", function (data, index) {
+            toolTip.hide(data);
+        });
+
+    // label it
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Poverty");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Age");
+    }).catch(function(error) {
+      console.log(error);
+})
