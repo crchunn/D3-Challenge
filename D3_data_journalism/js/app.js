b2ee9fc08569ee6e453527eaefbefb78726554e5
Carrie Chunn <crchunn@gmail.com>
2020-08-26 12:56:50 -0500

    setting up donuts

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -26,7 +26,50 @@ var margin = {
         data.healthcare = +data.healthcare;
         data.income = +data.income;
         data.obesity = +data.obesity;
+        data.smokes = +data.smokes;
     });
 
+    // setting up the scale
+
+    var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(stateData, d => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(stateData, d => d.age)])
+      .range([height, 0]);
+
+    //   axis
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // appendig the axes
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
     
+    // making the donuts
+
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cx", d => xLinearScale(d.healthcare))
+        .attr("cx", d => xLinearScale(d.income))
+        .attr("cy", d => yLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("cy", d => yLinearScale(d.obesity))
+        .attr("r", "15")
+        .attr("fill", "aqua")
+        .attr("opacity", ".5");
+
+    
+
+
+
   })
